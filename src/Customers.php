985c2fe96d56642dac8a7483d48c76e7a1f2017e
Adam Binnersley <abinnersley@gmail.com>
2019-07-01 13:47:02 +0100

    Bug fix

--- src/Customers.php
+++ src/Customers.php
@@ -77,7 +77,7 @@ class Customers extends \UserAuth\User{
         $customers = $this->db->selectAll($this->table_users, $additionalInfo, '*', ['regtime' => 'DESC'], [intval($start) => intval($limit)]);
         if(is_array($customers)){
             foreach($customers as $i => $customer){
-                if(isset($customer['county']) && is_numeric($customer['county'])){
+                if(isset($customer['county']) && is_numeric($customer['county']) && $customer['county'] >= 1){
                     $customers[$i]['county'] = $this->counties->getCountyName($customer['county']);
                 }
             }
